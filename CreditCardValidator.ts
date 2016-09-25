360da1cd7e50ae679c03d86bb269f58dea3ffe44
Shlomi <shgotlib@gmail.com>
2016-09-25 17:59:11 +0300

    Create CreditCardValidator.ts

---
+++ CreditCardValidator.ts
@@ -0,0 +1,36 @@
+import {Control} from 'angular2/common'
+
+export class CreditCardValidator {
+    static isLegalNumber(control: Control) {
+        var valid = false;
+        var cardNumber = control.value.toString().replace(/ +/g, '').replace(/-+/g, '');
+        var numDigits = control.value.toString().length;
+
+        if (cardNumber == "")
+            return;
+
+        if (numDigits >= 14 && numDigits <= 16) {
+            var sum = 0, i = numDigits - 1, pos = 1, digit, luhn = new String();
+            do {
+                digit = parseInt(cardNumber.charAt(i));
+                luhn += (pos++ % 2 == 0) ? digit * 2 : digit;
+            } while (--i >= 0)
+
+            for (i = 0; i < luhn.length; i++) {
+                sum += parseInt(luhn.charAt(i));
+            }
+            valid = sum % 10 == 0;
+        } else if (numDigits >= 8 && numDigits <= 9) {
+            var sum = 0;
+
+            for (var i = numDigits - 1, j = 1; i >= 0; i-- , j++) {
+                sum += ((cardNumber.charAt(i)) * j);
+            }
+            valid = sum % 11 == 0;
+        }
+
+        if (!valid) 
+            return { isLegalNumber: true };
+        return null;
+    }
+}
